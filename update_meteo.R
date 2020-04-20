285cff536d624f66e7a58f833a22b224b07e1d32
simonmoulds <sim.moulds@gmail.com>
2020-04-20 18:46:31 +0100

    make continuous loop

--- update_meteo.R
+++ update_meteo.R
@@ -1,26 +1,29 @@
 ## Author : Simon Moulds, Imperial College London
 ## Date   : May 2019
 
-meteo = read.csv("princeton_data.csv")
-nt = nrow(meteo)
-write.table(
-    meteo[1:30,,drop=FALSE],
-    "simulated_realtime_met_data.csv",
-    sep=",",
-    row.names=FALSE,
-    col.names=TRUE
-)
-
-i = 31
-while (i <= nt) {
+flag = TRUE
+while (flag) {
+    meteo = read.csv("princeton_data.csv")
+    nt = nrow(meteo)
     write.table(
-        meteo[i,,drop=FALSE],
+        meteo[1:30,,drop=FALSE],
         "simulated_realtime_met_data.csv",
-        append=TRUE,
         sep=",",
         row.names=FALSE,
-        col.names=FALSE
+        col.names=TRUE
     )
-    i = i+1
-    Sys.sleep(1)
+
+    i = 31
+    while (i <= nt) {
+        write.table(
+            meteo[i,,drop=FALSE],
+            "simulated_realtime_met_data.csv",
+            append=TRUE,
+            sep=",",
+            row.names=FALSE,
+            col.names=FALSE
+        )
+        i = i+1
+        Sys.sleep(5)
+    }
 }
